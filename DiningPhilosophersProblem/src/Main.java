89d58109ca81a16467214f8291c9f16fb12e1759
Sritan Vemuru <111458003+svemuru15@users.noreply.github.com>
2025-02-02 13:53:24 -0500

    Add files via upload

--- DiningPhilosophersProblem/src/Main.java
+++ DiningPhilosophersProblem/src/Main.java
@@ -15,10 +15,44 @@ public class Main {
             Object leftFork = forks[i];
             Object rightFork = forks[(i+1) % number_philosophers];
 
-            philosophers[i] = i == number_philosophers-1
+            philosophers[i] = (i == number_philosophers-1)
                     ? new Philosopher(rightFork, leftFork, (i+1))
                     : new Philosopher(leftFork, rightFork, (i+1));
             philosophers[i].start();
         }));
+    }
+}
+
+class Philosopher extends Thread{
+
+    private final Object left;
+    private final Object right;
+
+    Philosopher(Object left, Object right, int num) {
+        this.left = left;
+        this.right = right;
+        this.setName("Philosopher " + num);
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                act(" is thinking");
+                synchronized (left) {
+                    act(" picked up the left fork");
+                    synchronized (right) {
+                        act(" picked up the right fork and is eating");
+                    }
+                    act(" put down both forks, stopped eating and is thinking");
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void act(String msg) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + msg);
+        Thread.sleep(((int) (Math.random()*100)+50));
     }
 }
